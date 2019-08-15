361dd75f55a313f1725c7c58b8b35ef7fd9c9b03
kabucha <gavin-li@outlook.com>
2019-08-16 06:33:38 +0900

    add Flat Board shader

---
+++ UnityShader/Assets/Models/Shaders/Vertex_Fragment/FlatBoard.shader
@@ -0,0 +1,45 @@
+Shader "Hekelele3/FlatBoard"
+{
+	SubShader
+	{
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+				float4 color : COLOR;
+			};
+
+			v2f vert(appdata v)
+			{
+				v2f o = (v2f)0;
+				fixed blend = 2;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.color.r = (-v.vertex.x + v.vertex.z + blend)/(10+blend);
+				o.color.g = (-v.vertex.x - v.vertex.z + blend)/(10+blend);
+				o.color.b = ( v.vertex.x + v.vertex.z + blend)/(10+blend);
+				return o;
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				fixed4 col = i.color;
+				return col;
+			}
+			ENDCG
+		}
+	}
+
+}
---
+++ UnityShader/Assets/Scripts/PrintMeshVertex.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintMeshVertex : MonoBehaviour
+{
+
+    public Mesh mesh;
+    Vector3 minv, maxv;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        minv = Vector3.positiveInfinity;
+        maxv = Vector3.negativeInfinity;
+        foreach(var v in mesh.vertices)
+        {
+            print(v);
+            if (v.x < minv.x)
+            {
+                minv.x = v.x;
+            }
+            else if(v.x > maxv.x)
+            {
+                maxv.x = v.x;
+            }
+            if (v.y < minv.y)
+            {
+                minv.y = v.y;
+            }
+            else if (v.y > maxv.y)
+            {
+                maxv.y = v.y;
+            }
+            if (v.z < minv.z)
+            {
+                minv.z = v.z;
+            }
+            else if (v.z > maxv.z)
+            {
+                maxv.z = v.z;
+            }
+        }
+        string output = "min x: " + minv.x + "  min y: " + minv.y + "  min z: " + minv.z;
+        output += "\nmax x: " + maxv.x + "  max y: " + maxv.y + "  max z: " + maxv.z;
+        print(output);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
